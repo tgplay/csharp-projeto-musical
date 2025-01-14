e48ba518b1e07cddfb9da237d497671958ffb5a8
Tiago Martins <tiagoalmeidasp@hotmail.com>
2025-01-14 19:20:25 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!\n";
+List<string> listaDasBandas = new List<string>{
+    "Renascer Praise", "Cassiane", "Fernandinho"
+};
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+    ");
+    
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("1 - Registrar uma banda");
+    Console.WriteLine("2 - Listar bandas");
+    Console.WriteLine("3 - Avaliar uma banda");
+    Console.WriteLine("4 - Sair");
+    
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida); 
+    
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBanda();
+            break;
+        case 2: BandasRegistradas();            
+            break;
+        case 3:
+            Console.WriteLine("Avaliar uma banda");
+            break;
+        case 4:
+            Console.WriteLine("Sair");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("*******************");
+    Console.WriteLine("Registro de Bandas.");
+    Console.WriteLine("*******************\n");
+    Console.Write("\nDigite o nome da Banda: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A Banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void BandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("******************");
+    Console.WriteLine("Bandas Registradas");
+    Console.WriteLine("******************\n");
+    foreach (string banda in listaDasBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    /*for (int i=0; i < listaDasBandas.Count; i++)
+    {
+        Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    }*/    
+    Console.WriteLine("\nPressione qualquer tecla para voltar ao menu.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+ExibirOpcoesDoMenu();
